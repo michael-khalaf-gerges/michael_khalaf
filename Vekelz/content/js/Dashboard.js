42521872e6b7a2dec53cf6e81121503550d3ef17
michael-khalaf-gerges <115570244+michael-khalaf-gerges@users.noreply.github.com>
2023-01-09 00:22:42 +0200

    Add files via upload

---
+++ Vekelz/content/js/Dashboard.js
@@ -0,0 +1,6 @@
+import * as am4core from "@amcharts/amcharts4/core";
+import * as am4charts from "@amcharts/amcharts4/charts";
+$(document).ready(function () {
+
+
+});
---
+++ Vekelz/content/js/booking.js
@@ -0,0 +1,111 @@
+$(document).ready(function () {
+    AOS.init();
+
+    $menu = true;
+    $("#menu").click(function () {
+        if ($menu == true) {
+            $(".left-nav").addClass("d-block");
+            $menu = false;
+        } else {
+            $(".left-nav").removeClass("d-block");
+            $menu = true;
+        }
+    });
+    $('.car1-df').click(function () {
+        $('.car1-f').removeClass("d-none");
+        $('.car1-df').addClass("d-none");
+    });
+    $('.car1-f').click(function () {
+        $('.car1-df').removeClass("d-none");
+        $('.car1-f').addClass("d-none");
+    });
+    $('.car2-df').click(function () {
+        $('.car2-f').removeClass("d-none");
+        $('.car2-df').addClass("d-none");
+    });
+    $('.car2-f').click(function () {
+        $('.car2-df').removeClass("d-none");
+        $('.car2-f').addClass("d-none");
+    });
+    $('.car3-df').click(function () {
+        $('.car3-f').removeClass("d-none");
+        $('.car3-df').addClass("d-none");
+    });
+    $('.car3-f').click(function () {
+        $('.car3-df').removeClass("d-none");
+        $('.car3-f').addClass("d-none");
+    });
+    $('.car4-df').click(function () {
+        $('.car4-f').removeClass("d-none");
+        $('.car4-df').addClass("d-none");
+    });
+    $('.car4-f').click(function () {
+        $('.car4-df').removeClass("d-none");
+        $('.car4-f').addClass("d-none");
+    });
+    $('.car5-df').click(function () {
+        $('.car5-f').removeClass("d-none");
+        $('.car5-df').addClass("d-none");
+    });
+    $('.car5-f').click(function () {
+        $('.car5-df').removeClass("d-none");
+        $('.car5-f').addClass("d-none");
+    });
+    $('.car6-df').click(function () {
+        $('.car6-f').removeClass("d-none");
+        $('.car6-df').addClass("d-none");
+    });
+    $('.car6-f').click(function () {
+        $('.car6-df').removeClass("d-none");
+        $('.car6-f').addClass("d-none");
+    });
+    $('.car7-df').click(function () {
+        $('.car7-f').removeClass("d-none");
+        $('.car7-df').addClass("d-none");
+    });
+    $('.car7-f').click(function () {
+        $('.car7-df').removeClass("d-none");
+        $('.car7-f').addClass("d-none");
+    });
+    $('.car8-df').click(function () {
+        $('.car8-f').removeClass("d-none");
+        $('.car8-df').addClass("d-none");
+    });
+    $('.car8-f').click(function () {
+        $('.car8-df').removeClass("d-none");
+        $('.car8-f').addClass("d-none");
+    });
+    $('.car9-df').click(function () {
+        $('.car9-f').removeClass("d-none");
+        $('.car9-df').addClass("d-none");
+    });
+    $('.car9-f').click(function () {
+        $('.car9-df').removeClass("d-none");
+        $('.car9-f').addClass("d-none");
+    });
+    $('.car10-df').click(function () {
+        $('.car10-f').removeClass("d-none");
+        $('.car10-df').addClass("d-none");
+    });
+    $('.car10-f').click(function () {
+        $('.car10-df').removeClass("d-none");
+        $('.car10-f').addClass("d-none");
+    });
+    $('.car11-df').click(function () {
+        $('.car11-f').removeClass("d-none");
+        $('.car11-df').addClass("d-none");
+    });
+    $('.car11-f').click(function () {
+        $('.car11-df').removeClass("d-none");
+        $('.car11-f').addClass("d-none");
+    });
+    $('.car12-df').click(function () {
+        $('.car12-f').removeClass("d-none");
+        $('.car12-df').addClass("d-none");
+    });
+    $('.car12-f').click(function () {
+        $('.car12-df').removeClass("d-none");
+        $('.car12-f').addClass("d-none");
+    });
+
+})
